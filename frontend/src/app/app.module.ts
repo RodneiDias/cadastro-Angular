7fdd3c2603b053de6f8078a43b42c3dd991861b7
Rodnei Dias <rodnei.dias4@gmail.com>
2022-04-28 16:45:36 -0300

    Criação event biden

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,8 +12,9 @@ import{MatSidenavModule} from '@angular/material/sidenav'
 import{MatListModule} from '@angular/material/list';
 import{MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
-
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import{MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
