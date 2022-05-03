b1170b59f973cbe68091660909dc3d88afdff1b2
Rodnei Dias <rodnei.dias4@gmail.com>
2022-05-03 16:05:09 -0300

    Criação
    Criação da lista de produtos

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product-model';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-read',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-read.component.css']
 })
 export class ProductReadComponent implements OnInit {
+  
+  products!: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+this.productService.read().subscribe(products =>{
+  this.products = products
+  console.log(products)
+})
+
   }
 
 }
